7814a59e6fcc03652b1a9f09acbe4b12567630fa
Iziogo Emmanuel Ugochukwu <eugochukwu77@gmail.com>
2024-10-10 08:32:30 +0100

    Update settings.py

--- Hospital_Management/settings.py
+++ Hospital_Management/settings.py
@@ -149,8 +149,8 @@ EMAIL_BACKEND='django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST='smtp.gmail.com'
 EMAIL_PORT=587
 EMAIL_USE_TLS=True
-EMAIL_HOST_USER='eugochukwu77@gmail.com'
-EMAIL_HOST_PASSWORD='tylt twfz lpbj jgvd'
+EMAIL_HOST_USER='email'
+EMAIL_HOST_PASSWORD='app password'
 
 
 AUTHENTICATION_BACKENDS = [
